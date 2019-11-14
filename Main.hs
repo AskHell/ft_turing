260a002c659ca35f95135544cdaf3b98906bc099
erobert <etiennerobert33@gmail.com>
2019-11-15 00:19:31 +0100

    get whole machine description

--- Main.hs
+++ Main.hs
@@ -1,15 +1,28 @@
-import Turing (Machine(..))
+import Turing
 
 import qualified Data.ByteString.Lazy as B
 
-import Data.Aeson (encode)
+-- import qualified Data.Map.Strict as Map
+-- main = putStrLn $ show $ encode
+  -- Turing.Machine {
+    -- name="Test",
+    -- alphabet=["a"],
+    -- blank=".",
+    -- states=["state1", "state2"],
+    -- initial="state1",
+    -- finals=["state2"],
+    -- transitions=Map.fromList [("t1", Turing.Transition {
+      -- Turing.read="state1",
+      -- to_state="state2",
+      -- write="a",
+      -- action=Turing.Left
+    -- })]
+  -- }
 
-main = putStrLn $ show $ encode
-  Machine {
-    name="Test",
-    alphabet=["a"],
-    blank=".",
-    states=["state1", "state2"],
-    initial="state1",
-    finals=["state2"]
-  }
+getOutput :: Either String Machine -> String
+getOutput (Prelude.Left s) = s
+getOutput (Prelude.Right m) = show $ encode m
+
+main = do
+  file_content <- B.readFile "test.json"
+  putStrLn $ getOutput $ eitherDecode file_content
--- Turing.hs
+++ Turing.hs
@@ -4,14 +4,35 @@
 
 module Turing
 (
-  Machine (..)
+  Machine,
+  encode,
+  eitherDecode
 ) where
 
 import GHC.Generics (Generic)
+import Data.ByteString.Lazy (ByteString)
 import Data.Aeson (FromJSON, ToJSON)
+import qualified Data.Aeson as Aeson (encode, eitherDecode)
+
+import Data.Map.Strict (Map)
 
 type Letter = String
 type State = String
+
+data Action = LEFT | RIGHT deriving (Generic)
+instance FromJSON Action
+instance ToJSON Action
+
+data Transition =
+  Transition {
+    read :: State,
+    to_state :: State,
+    write :: Letter,
+    action :: Action
+  } deriving (Generic)
+
+instance FromJSON Transition
+instance ToJSON Transition
 
 data Machine =
   Machine {
@@ -20,8 +41,15 @@ data Machine =
     blank :: Letter,
     states :: [State],
     initial :: State,
-    finals :: [State]
+    finals :: [State],
+    transitions :: Map String [Transition]
   } deriving (Generic)
 
 instance FromJSON Machine
 instance ToJSON Machine
+
+encode :: Machine -> ByteString
+encode = Aeson.encode
+
+eitherDecode :: ByteString -> Either String Machine
+eitherDecode = Aeson.eitherDecode
