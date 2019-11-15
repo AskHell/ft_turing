e816794033784e8c6ade73971680546e2b427080
erobert <etiennerobert33@gmail.com>
2019-11-15 13:13:45 +0100

    Add tests to decode

--- Turing.hs
+++ Turing.hs
@@ -48,8 +48,22 @@ data Machine =
 instance FromJSON Machine
 instance ToJSON Machine
 
+valid :: Machine -> Either String Machine
+valid m
+  | not $ all (\l -> length l == 1) $ alphabet m =
+    Left "Error: Every character from the alphabet must be a string of size 1"
+  | notElem (blank m) (alphabet m) =
+    Left "Error: The blank character must be part of the alphabet"
+  | notElem (initial m) (states m) =
+    Left "Error: The initial state must be part of the states"
+  | not $ all (\x -> elem x $ states m) $ finals m =
+    Left "Error: All the final states must be part of the states"
+  -- TODO Test if all to_state from every transition is in Machine states
+  -- TODO Test if all write from every transition is in Machine alphabet
+valid m = return m
+
 encode :: Machine -> ByteString
 encode = Aeson.encode
 
 eitherDecode :: ByteString -> Either String Machine
-eitherDecode = Aeson.eitherDecode
+eitherDecode bs = Aeson.eitherDecode bs >>= valid
