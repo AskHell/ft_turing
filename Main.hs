fa195a6ff0aee4b1de11915ca2c446d22a9bfd1e
Failte21 <lsimon@student.42.fr>
2019-11-29 11:05:32 +0100

    IOParsing: Create parsing with fairly explicit error messages

--- Main.hs
+++ Main.hs
@@ -1,41 +1,12 @@
-import TuringMachine (eitherDecode)
 import Control.Exception (catch, displayException)
 import System.IO
 import System.IO.Error
 import System.Environment
 import qualified Data.ByteString.Lazy as B (readFile, ByteString)
-
 import Debug.Trace (trace)
 
--- import qualified Data.Map.Strict as Map
--- main = putStrLn $ show $ encode
-  -- Turing.Machine {
-    -- name="Test",
-    -- alphabet=["a"],
-    -- blank=".",
-    -- states=["state1", "state2"],
-    -- initial="state1",
-    -- finals=["state2"],
-    -- transitions=Map.fromList [("t1", Turing.Transition {
-      -- Turing.read="state1",
-      -- to_state="state2",
-      -- write="a",
-      -- action=Turing.Left
-    -- })]
-  -- }
-
--- getOutput :: Either String Machine -> String
--- getOutput (Prelude.Left s) = s
--- getOutput (Prelude.Right m) = show $ encode m
-
-usage :: IO()
-usage = putStrLn $ "Usage: ./ft_turing <description_file> <input>"
-
-parseArgs :: [String] -> IO (Either String (B.ByteString, String))
-parseArgs (filename:input:[]) = do
-  description <- B.readFile filename
-  return $ Right (description, input)
-parseArgs _ = return $ Left "Missing args"
+import TuringMachine (eitherDecode)
+import IOParsing (parseArgs, usage)
 
 main :: IO ()
 main = toTry `catch` handler
@@ -45,11 +16,8 @@ toTry = do
   args <- getArgs
   action <- parseArgs args
   case action of
-    Left error -> usage
+    Left invalid -> usage invalid
     Right (description, input) -> putStrLn "Hooray"
-
-  -- let result = ft_turing description input
-  -- final result
 
 handler :: IOError -> IO ()
 handler e = putStrLn $ displayException e
